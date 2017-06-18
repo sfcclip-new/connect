282ffa2e74c8a17da81c6633485fa05d0ce0d4e0
Akio Yasui <akkyie01@gmail.com>
2017-06-18 09:56:28 +0900

    production時のbaseURLを修正

--- console/src/api/index.js
+++ console/src/api/index.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL = (process.env['NODE_ENV'] == 'production'
+  ? '/api'
+  : 'http://localhost:8080/api'
+)
+
 export default axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/vnd.api+json',
     'Accept': 'application/vnd.api+json'
